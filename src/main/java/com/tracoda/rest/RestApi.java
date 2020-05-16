27bcc7aea30e7d57faae10ceb526a9ac8e80392d
javierbeltran425 <00019315@uca.edu.sv>
2020-05-16 14:58:02 -0600

    se quitan los cors

--- src/main/java/com/tracoda/rest/RestApi.java
+++ src/main/java/com/tracoda/rest/RestApi.java
@@ -15,25 +15,25 @@ public class RestApi {
     @Autowired
     FilesController filesController;
 
-    @CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
+    //@CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
     @RequestMapping(value={"files/all"}, method = RequestMethod.GET)
     public Iterable<Files> getAllFiles(){
         return filesController.getAllFiles();
     }
 
-    @CrossOrigin(origins="https://tracodafronttest.herokuapp.com/")
+    //@CrossOrigin(origins="https://tracodafronttest.herokuapp.com/")
     @RequestMapping(value = {"solicitud/archivo/save"}, method = RequestMethod.PUT)
     public void saveSolicitudXfoto(@RequestBody MultipartFile dr) throws Exception {
         filesController.saveFotoXSolicitud(dr);
     }
 
-    @CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
+    //@CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
     @RequestMapping(value = {"solicitud/archivo/mes"}, method = RequestMethod.GET)
     public Iterable<Files> getSolicitudXmes(@RequestParam BigDecimal mes) throws Exception {
         return filesController.getFilesXSolicitudMes(mes);
     }
 
-    @CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
+    //@CrossOrigin(origins="https://tracodafronttest.herokuapp.com")
     @RequestMapping(value = {"solicitud/archivo/mesAnio"}, method = RequestMethod.GET)
     public void getSolicitudXmesAnio(@RequestParam BigDecimal mes, BigDecimal anio) throws Exception {
         filesController.getFilesXSolicitudMesAnio(mes,anio);
